bde201aafb42603edc4d3c3ad387496b705ce472
Artöm Bakri Al-Sarmini <svsfvsfvcfssfvffs@gmail.com>
2017-12-04 22:25:22 +0300

    WIP

--- src/Windows/CodeWindow.cs
+++ src/Windows/CodeWindow.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 using Befunge.Interpreter;
 
@@ -11,14 +12,42 @@ namespace Befunge.Windows
 		private Stack<long> _stack;
 		private List<List<long>> _field;
 
+		private CaretPosition _caretPosition;
+
 		public void Run()
 		{
-			if (_ip    == null) _ip    = new InstructionPointer();
-			if (_stack == null) _stack = new Stack<long>();
-			if (_field == null) _field = new List<List<long>>();
+			if (_ip            == null) _ip            = new InstructionPointer();
+			if (_stack         == null) _stack         = new Stack<long>();
+			if (_field         == null) _field         = CreateEmptyField();
+			if (_caretPosition == null) _caretPosition = new CaretPosition();
+			
+			while (true)
+			{
+				var k = Console.ReadKey();
+				switch (k.Key)
+				{
+					case ConsoleKey.RightArrow:
+						_caretPosition.X++;
+						break;
+					case ConsoleKey.LeftArrow:
+						_caretPosition.X--;
+						break;
+					case ConsoleKey.UpArrow:
+						_caretPosition.Y++;
+						break;
+					case ConsoleKey.DownArrow:
+						_caretPosition.Y--;
+						break;
+					default:
+					{
+						char c = k.KeyChar;
+						break;
+					}
+				}
+			}
 		}
 
-		public List<List<long>> CreateEmptyField()
+		private List<List<long>> CreateEmptyField()
 		{
 			var row = new List<long>(
 				new long [] { 32, 32, 32, 32, 32, 32, 32, 32,
@@ -29,6 +58,37 @@ namespace Befunge.Windows
 			return field;
 		}
 
+		private void PrintField()
+		{
+			for (int i = 0; i < 16; i++)
+				for (int j = 0; j < 16; j++)
+					Console.WriteLine((char)
+						((_field[i][j] > 31 && _field[i][j] < 35)
+							? _field[i][j]
+							: 0xFFFD));
+		}
+
+		private bool IsInstruction(char c)
+		{
+			return c > 31 && c < 127;
+		}
+
 		public WindowType Type => WindowType.Code;
+
+		class CaretPosition
+		{
+			private int _x, _y;
+			public int X
+			{
+				get => _x;
+				set => _x = value;
+			}
+
+			public int Y
+			{
+				get => _y;
+				set => _y = value;
+			}
+		}
 	}
 }
